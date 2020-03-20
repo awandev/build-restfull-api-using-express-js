526b4d723983f673a922c14543c2bb70e9d16102
awalone <migoyeng@gmail.com>
2020-03-21 00:55:18 +0800

    penambahan route

--- server/app.js
+++ server/app.js
@@ -6,7 +6,13 @@ const dotenv = require('dotenv')
 dotenv.config({path: './config/config.env'})
 
 
+// importing all routes
+const jobs = require('./routes/jobs')
+app.use('/api/v1', jobs);
+
+
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+    console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 })
