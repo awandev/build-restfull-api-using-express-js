4180da0429a0eae0a8328662b803674feac21efe
awalone <migoyeng@gmail.com>
2020-03-21 15:07:15 +0800

    get all jobs with results jobs.length

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -2,10 +2,14 @@ const Job = require('../models/jobs')
 
 
 // get all jobs => /api/v1/jobs
-exports.getJobs = (req, res, next) => {
+exports.getJobs = async(req, res, next) => {
+
+    const jobs = await Job.find();
+
     res.status(200).json({
         success: true,
-        message: 'This route will display all jobs in future'
+        results: jobs.length,
+        data : jobs
     });
 } 
 
