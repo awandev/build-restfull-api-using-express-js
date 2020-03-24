98449b452a54b85b39ef1fe189e4580d2056fe0c
awalone <migoyeng@gmail.com>
2020-03-24 14:21:44 +0800

    adding sort filter

--- server/utils/apiFilters.js
+++ server/utils/apiFilters.js
@@ -7,12 +7,35 @@ class APIFilters {
     filter() {
         const queryCopy = {...this.queryStr};
 
+        // removing fields from the query
+        const removeFields = ['sort'];
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        console.log(queryCopy);
+
+
         // advanced filter using lt, lte, gt, gte
         let queryStr = JSON.stringify(queryCopy);
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
         console.log(queryStr);
         this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    sort() {
+        if(this.queryStr.sort) {
+            // if in search have many arguement, so split it with comma
+            // example jobs?sort=salary,jobType
+            // sort by salary and jobTYpe
+            const sortBy = this.queryStr.sort.split(',').join(' ');
+            console.log(sortBy);
+            this.query = this.query.sort(this.queryStr.sort);
+        }
+        else {
+            // if no sort option, sort it by postingDate (by default)
+            this.query = this.query.sort('-postingDate');
+        }
         return this;
     }
 }
